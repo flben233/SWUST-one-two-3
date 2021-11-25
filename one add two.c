ccaeb23807d29ddecaef252d980a47e653053f38
ShirakawaTyu <40626158+flben233@users.noreply.github.com>
2021-11-25 16:03:18 +0800

    Create one add two.c

---
+++ one add two.c
@@ -0,0 +1,109 @@
+/*
+ * Problem: 1550-L (one + two = 3 数组 难度3)
+ * User: 5120210510
+ * Language: Clang18
+ * Result: Accepted
+ * Time: 3 MS  Memory: 3396 KB
+ */
+#include<stdio.h>
+
+int main()
+{
+    char a[100] = { '\0' }, gc[100];
+    int i = 0, b = 0, c = 0, d = 0, e = 0, f = 0, jieguo = 0;
+    while (1) {
+        if (jieguo != 0) {           //用于清除scanf缓冲区
+            scanf("%s", gc);
+            strcpy(gc, "");
+        }
+        i = 0, b = 0, c = 0, d = 0, e = 0, f = 0, jieguo = 0;
+        strcpy(a, "");        //用于清空数组a中数据
+        char t[5] = "ooooo";
+        //***************以上为初始化操作*******************
+        for (i = 0; i < 5; i++) {
+            scanf("%s", a);
+            if (a[0] == '=') {
+                break;
+            }
+            else {
+                if (strcmp(a, "one") == 0) {
+                    t[b] = 1;
+                    b++;
+                }
+                else if (strcmp(a, "two") == 0) {
+                    t[b] = 2;
+                    b++;
+                }
+                else if (strcmp(a, "three") == 0) {
+                    t[b] = 3;
+                    b++;
+                }
+                else if (strcmp(a, "four") == 0) {
+                    t[b] = 4;
+                    b++;
+                }
+                else if (strcmp(a, "five") == 0) {
+                    t[b] = 5;
+                    b++;
+                }
+                else if (strcmp(a, "six") == 0) {
+                    t[b] = 6;
+                    b++;
+                }
+                else if (strcmp(a, "seven") == 0) {
+                    t[b] = 7;
+                    b++;
+                }
+                else if (strcmp(a, "eight") == 0) {
+                    t[b] = 8;
+                    b++;
+                }
+                else if (strcmp(a, "nine") == 0) {
+                    t[b] = 9;
+                    b++;
+                }
+                else if (strcmp(a, "zero") == 0) {
+                    t[b] = 0;
+                    b++;
+                }
+                else if (strcmp(a, "+") == 0) {
+                    t[b] = '+';
+                    b++;
+                }
+            }
+        }
+        for (c = 0; c < 2; c++) {   //判断加号前的数字的位数
+            if (t[c] == '+') {
+                break;
+            }
+        }
+        if (c == 1) {    //将加号前的数字按照位数还原
+            d = t[0];
+        }
+        else if (c == 2) {
+            d = t[0] * 10 + t[1];
+        }
+        while (c < 4) {   //判断加号后的数字的位数
+            c++;
+            if (t[c] == 'o') {
+                c--;
+                break;
+            }
+            f++;
+        }
+        if (f == 1) {   ////将加号后的数字按照位数还原
+            e = t[c];
+        }
+        else if (f == 2) {
+            e = t[c - 1] * 10 + t[c];
+        }
+        if (d == 0 && e == 0) {    //如果两数均为zero则结束
+            break;
+        }
+        else {      //否则输出结果
+            jieguo = d + e;
+            printf("%d\n", jieguo);
+        }
+    }
+    return 0;
+}
